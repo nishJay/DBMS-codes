77442bde2b86037dae17a8d55ebb646a68d0510d
R JAYANTH JADHAV <77871395+nishJay@users.noreply.github.com>
2022-11-14 16:10:55 +0530

    Update Ex3.sql

--- Ex3.sql
+++ Ex3.sql
@@ -29,6 +29,7 @@ insert into sailor values('S2','Shyam',34,'4');
 insert into sailor values('S3','Jam',25,'2');
 insert into sailor values('S4','Nam',25,'1');
 insert into sailor values('S5','Dam',30,'4');
+insert into sailor values('S6','Tiny',30,'4');
 
 insert into Reserves values('B1','S2','MON');
 insert into Reserves values('B2','S4','TUE');
@@ -36,9 +37,17 @@ insert into Reserves values('B3','S3','WED');
 insert into Reserves values('B4','S1','MON');
 insert into Reserves values('B5','S5','MON');
 
-# Obtain the details of the boats reserved by ‘#Sailor_Name’.
+# Obtain the details of the boats reserved by ‘Shyam’.
 select Boat_id, BoatName,Boat_colour from boat where Boat_id in (select Boat_id from Reserves where Sailor_id in (select Sailor_id from sailor where Sailor_name = 'Shyam'));
 
+# Retrieve the BID of the boats reserved necessarily by all the sailors
+select Boat_id from reserves;
+
+# Find the number of boats reserved by each sailor. Display the Sailor_Name along with the number of boats reserved
+select count(Boat_id), sailor.Sailor_name from Reserves join sailor on Reserves.Sailor_id = sailor.Sailor_id group by sailor.Sailor_Name;
+
+# Identify which boats have the same name as their sailor.
+select Boatname, sailor.Sailor_name from boat join sailor on boat.Boatname = sailor.Sailor_Name;  
 
 
 
