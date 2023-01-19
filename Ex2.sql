d7f13daac621911b8aa92d6d2966f3829b09bd56
R JAYANTH JADHAV <77871395+nishJay@users.noreply.github.com>
2023-01-19 23:28:59 +0530

    Update Ex2.sql

--- Ex2.sql
+++ Ex2.sql
@@ -5,10 +5,22 @@
 #shipped, respectively. The Shipment relation should contain information on the number of parts
 #shipped
 
-Create table supplier(sid varchar2(20) not null primary key,  sname char(20),  saddr varchar2(20));  
-Create table part(pid varchar2(20) not null primary key,  pname char(20),  pcolor varchar2(20));  
-Create table shipment(sid varchar2(20) references supplier(sid),  pid varchar2(20) references part(pid),no_ship number, primary key(sid,pid));  
+CREATE TABLE PART( 
+  PID VARCHAR(20) NOT NULL PRIMARY KEY,
+  PNAME CHAR(20),
+  PCOLOR CHAR(20));
 
+CREATE TABLE SUPPLIER(
+  SID VARCHAR(20) NOT NULL PRIMARY KEY,
+  SNAME CHAR(20),
+  SADDR VARCHAR(50));
+  
+  CREATE TABLE SUPPLY(
+    PID VARCHAR(20) REFERENCES PART(PID),
+    SID VARCHAR(20) REFERENCES SUPPLIER(SID),
+    QUANT INTEGER,
+  PRIMARY KEY(PID,SID));
+    
 insert into supplier values('S1','AA','AAA');
 insert into supplier values('S2','AB','BBB');
 insert into supplier values('S3','AC','CCC');
@@ -19,44 +31,53 @@ insert into part values('P2','DB','Blue');
 insert into part values('P3','DC','Brown');
 insert into part values('P4','DD','Beige');
 
-insert into shipment values('S1','P1',25);
-insert into shipment values('S2','P2',26);
-insert into shipment values('S3','P3',27);
-insert into shipment values('S4','P4',28);
-insert into shipment values('S1','P2',29);
-insert into shipment values('S1','P3',30);
-insert into shipment values('S1','P4',31);
-
-#Obtain the details of parts supplied by supplier 'S1'
-select * from part where pid in (select pid from shipment where sid in ('S1'));
+insert into supply values('P1','S1',25);
+insert into supply values('P2','S2',26);
+insert into supply values('P3','S3',27);
+insert into supply values('P4','S4',28);
+insert into supply values('P1','S2',29);
+insert into supply values('P1','S3',30);
+insert into supply values('P1','S4',31);
 
 
-PID		     PNAME		  PCOLOR
--------------------- -------------------- --------------------
-P1		     DA 		  Black
-P2		     DB 		  Blue
-P3		     DC 		  Brown
-P4		     DD 		  Beige
+#Obtain the details of parts supplied by supplier 'AA'
+SELECT *
+FROM PART
+WHERE PID IN ( SELECT PID
+               FROM SUPPLY
+               WHERE SID IN ( SELECT SID 
+                              FROM SUPPLIER
+                              WHERE SNAME = 'AA'));
+
+
 
 #Obtain the Names of suppliers who supply 'P2'.
-select sname from supplier where sid in (select sid from shipment where pid in ('P2'));
+SELECT SNAME
+FROM SUPPLIER 
+WHERE SID IN ( SELECT SID 
+               FROM SUPPLY
+               WHERE PID IN ( SELECT PID 
+                              FROM PART
+                              WHERE PNAME = 'DA'));
 
-SNAME
---------------------
-AA
-AB
 
 #Delete the parts which are in 'Brown'
-delete from shipment where pid in (select pid from part where pcolor = 'Brown');
-2 rows deleted.
+DELETE 
+FROM SUPPLY
+WHERE PID IN ( SELECT PID 
+               FROM PART 
+               WHERE PCOLOR = 'Beige');
+DELETE
+FROM PART
+WHERE PCOLOR = 'Beige';
+
+SELECT *
+FROM PART;
 
 #List the suppliers who supplies exactly two parts.
-insert into shipment values('S3','P3',2);
-insert into shipment values('S3','P4',2);
-insert into shipment values('S4','P2',2);
-select sid, count(sid) from shipment having count(sid) = 2 group by sid;
-
-SID		     COUNT(SID)
--------------------- ----------
-S4			      2
+SELECT S.SID, S.SNAME, COUNT(SU.SID) AS "NO. OF KINDS OF PARTS SUPPLIED"
+FROM SUPPLIER S JOIN SUPPLY SU
+ON S.SID = SU.SID
+GROUP BY SU.SID
+HAVING "NO. OF KINDS OF PARTS SUPPLIED" = 2;
 
